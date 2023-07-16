094a4f05226f41647f05c6e2932aa6718f6bb3ec
Akrati Verma <128223364+blindaks@users.noreply.github.com>
2023-07-16 20:26:19 +0530

    16 July 2023

---
+++ Queue Reversal.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        stack<int>s;
+        while(!q.empty())
+        {
+            s.push(q.front());
+            q.pop();
+        }
+        while(!s.empty())
+        {
+            q.push(s.top());
+            s.pop();
+        }
+        return q;
+        // add code here.
+    }
+};
