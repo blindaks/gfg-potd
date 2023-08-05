6218e03a76f51124e0a771c1ab087d79ee886137
Akrati Verma <128223364+blindaks@users.noreply.github.com>
2023-08-05 10:45:36 +0530

    05 Aug 2023

---
+++ Chocolate Distribution Problem.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+        sort(a.begin(),a.end());
+        long long ans=INT_MAX;
+        for(int i=0;i+m-1<n;i++)
+        {
+            ans=min(ans,a[i+m-1]-a[i]);
+        }
+        return ans;
+    }   
+};
