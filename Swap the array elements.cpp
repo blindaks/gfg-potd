f5c1837deba67176a2145f5dc26ff0135c48d9c0
Akrati Verma <128223364+blindaks@users.noreply.github.com>
2024-03-04 18:00:33 +0530

    04 Mar 2024

---
+++ Swap the array elements.cpp
@@ -0,0 +1,15 @@
+//User function Template for C++
+
+/*Function to swap array elements
+* arr : array input
+* n : number of elements in array
+*/
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        // Your code here
+        for(int i=0;i<n-2;i++)
+        swap(arr[i],arr[i+2]);
+    }
+};
